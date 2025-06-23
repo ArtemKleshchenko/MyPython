a9f190c02aa2bfd25e20097d238974c09e5dbef8
АРТЕМ <kleshchenko25@gmail.com>
2025-06-23 13:47:14 +0300

    commit 1

--- HW3_2.py
+++ HW3_2.py
@@ -1,9 +1,4 @@
-lst = []
-lst.append(2)
-lst.append(9.3)
-lst.append([947, 2])
-lst.append([999, 000])
-print(lst)
-lst.pop(-1)
-lst.insert(0, [999, 000])
+lst = [2, 9.3, [947, 2], [999, 0]]
+lst.remove([999, 0])
+lst.insert(0, [999, 0])
 print(lst)
