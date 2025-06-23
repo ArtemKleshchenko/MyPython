172d23adabf997e3503c8c7d552e11d818bac767
АРТЕМ <kleshchenko25@gmail.com>
2025-06-23 09:42:03 +0300

    commit 1

--- HW3_2.py
+++ HW3_2.py
@@ -1,6 +1,18 @@
-def last_to_first(lst):
-    if len(lst) > 1:
-        return [lst[-1]] + lst[:-1]
-    return lst
+list = [
+    [99, 43, 68, 1, 22, 653]
+]
 
-print(last_to_first([5, 9, 3, 12, 49, 13, 23, 65, 81]))
+i = 0
+while i < len(list):
+    lst = list[i]
+
+    if len(lst) > 1:
+        last = lst[-1]
+        j = len(lst) - 1
+        while j > 0:
+            lst[j] = lst[j - 1]
+            j -= 1
+        lst[0] = last
+
+    print(lst)
+    i += 1
--- HW3_3.py
+++ HW3_3.py
@@ -1,5 +1,24 @@
-def split_list(lst):
-    middle = (len(lst) + 1) // 2
-    return [lst[:middle], lst[middle:]]
+lst = [99, 43, 68, 1, 22, 653]
 
-print(split_list([99, 43, 68, 1, 22, 653]))
+if len(lst) == 0:
+    result = [[], []]
+else:
+    if len(lst) % 2 == 0:
+        half = len(lst) // 2
+    else:
+        half = len(lst) // 2 + 1
+
+    left = []
+    right = []
+
+    i = 0
+    while i < len(lst):
+        if i < half:
+            left += [lst[i]]
+        else:
+            right += [lst[i]]
+        i += 1
+
+    result = [left, right]
+
+print(result)
