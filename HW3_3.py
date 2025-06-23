12a79a3e321a6764fdcb74f7a8f5e3a75049f224
АРТЕМ <kleshchenko25@gmail.com>
2025-06-23 14:14:54 +0300

    commit 1

--- HW3_3.py
+++ HW3_3.py
@@ -1,24 +1,3 @@
-lst = [99, 43, 68, 1, 22, 653]
-
-if len(lst) == 0:
-    result = [[], []]
-else:
-    if len(lst) % 2 == 0:
-        half = len(lst) // 2
-    else:
-        half = len(lst) // 2 + 1
-
-    left = []
-    right = []
-
-    i = 0
-    while i < len(lst):
-        if i < half:
-            left += [lst[i]]
-        else:
-            right += [lst[i]]
-        i += 1
-
-    result = [left, right]
-
-print(result)
+lst = [2, 9.3, 89, [947, 2], 54, [999, 0]]
+lst = [lst[:3], lst[3:]]
+print(lst)
