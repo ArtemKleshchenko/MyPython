388d3dafdc1105a6f58786e91f539962f04769ff
АРТЕМ <kleshchenko25@gmail.com>
2025-06-23 11:25:44 +0300

    commit 1

--- HW3_2.py
+++ HW3_2.py
@@ -1,18 +1,9 @@
-list = [
-    [99, 43, 68, 1, 22, 653]
-]
-
-i = 0
-while i < len(list):
-    lst = list[i]
-
-    if len(lst) > 1:
-        last = lst[-1]
-        j = len(lst) - 1
-        while j > 0:
-            lst[j] = lst[j - 1]
-            j -= 1
-        lst[0] = last
-
-    print(lst)
-    i += 1
+lst = []
+lst.append(2)
+lst.append(9.3)
+lst.append([947, 2])
+lst.append([999, 000])
+print(lst)
+lst.pop(-1)
+lst.insert(0, [999, 000])
+print(lst)
