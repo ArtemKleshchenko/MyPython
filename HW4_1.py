4a83494e3b12f533a7500b4951217df1521b866c
АРТЕМ <kleshchenko25@gmail.com>
2025-06-26 12:15:11 +0300

    commit 1

--- HW4_1.py
+++ HW4_1.py
@@ -1,12 +1,12 @@
 lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-result = []
+new_lst = []
 
 for x in lst:
     if x != 0:
-        result.append(x)
+        new_lst.append(x)
 
 zero_count = lst.count(0)
 for _ in range(zero_count):
-    result.append(0)
+    new_lst.append(0)
 
-print(result)
+print(new_lst)
