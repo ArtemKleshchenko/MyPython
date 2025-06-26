49c960adb5a34460ae25a281d3a59bb6a3fbfe81
АРТЕМ <kleshchenko25@gmail.com>
2025-06-26 09:03:37 +0300

    commit 1

--- HW4_3.py
+++ HW4_3.py
@@ -1,3 +1,10 @@
-lst = [9, 7, 31, 45, 23, 75, 96]
+import random
+
+length = random.randint(3, 10)
+
+lst = [random.randint(0, 100) for _ in range(length)]
+
 new_lst = [lst[0], lst[2], lst[-2]]
+
+print(lst)
 print(new_lst)
