bf03f7b28f1b1bddc6857f19945198b6e59b2abc
АРТЕМ <kleshchenko25@gmail.com>
2025-07-08 14:31:10 +0300

    commit 1

--- HW7_4.py
+++ HW7_4.py
@@ -3,5 +3,5 @@ def common_elements():
     divisible_by5 = {num for num in range(100) if num % 5 == 0}
     return divisible_by3 & divisible_by5
 
-assert common_elements() == {0, 15, 30, 45, 60, 75, 90}
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
 print('OK')
